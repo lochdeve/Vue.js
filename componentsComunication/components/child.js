49ed37e2ea2f9d48a5c36962e45323783abc2403
Carlos Garcia <alu0101208268.ull.edu.es>
2022-03-19 21:55:26 +0000

    comunication from child to parent

--- componentsComunication/components/child.js
+++ componentsComunication/components/child.js
@@ -4,9 +4,18 @@ Vue.component('child', {
     <div class="py-5 bg-success text-white">
       <h2>Child component</h2>
       <h5>Parent Age: {{age}}</h5>
+      <h5>Name: {{name}}</h5>
     </div>
   
   `,
-  props: ['age']
+  props: ['age'],
+  data() {
+    return {
+      name: 'Pedro'
+    }
+  },
+  mounted() {
+    this.$emit('childName', this.name)
+  }
 
 });
--- componentsComunication/components/parent.js
+++ componentsComunication/components/parent.js
@@ -4,14 +4,16 @@ Vue.component('parent', {
     <div class="p-5 bg-dark text-white">
       <h2>Parent component </h2>
       <h5>Age: {{parentAge}}</h5>
+      <h5>Chil Name: {{parentName}}</h5>
       <button class="btn btn-success btn-sm" @click="parentAge++">+</button>
       <button class="btn btn-danger btn-sm" @click="parentAge--">-</button>
-      <child :age="parentAge"></child>
+      <child :age="parentAge" @childName="parentName = $event"></child>
     </div>
   `,
   data() {
     return {
-      parentAge: 40
+      parentAge: 40,
+      parentName: ''
     }
   }
 
